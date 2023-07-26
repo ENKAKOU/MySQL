6a16a4966bf92c6d3e68baa5c06c4715ff704c4b
enkakou <enkakou0716@gmail.com>
2023-07-26 13:26:17 +0900

    Exercise_07

--- SGG-MySQL/Base/chapter_07/Exercise_07.sql
+++ SGG-MySQL/Base/chapter_07/Exercise_07.sql
@@ -1,5 +1,5 @@
 # 1.显示系统时间(注: 日期+时间)
-  SELECT NOW(),SYSDATE(),CURRENT_TIMESTAMP(),LOCALTIME(),LOCALTIMESTAMP()
+  SELECT NOW(),SYSDATE(),CURRENT_TIMESTAMP(),LOCALTIME(),LOCALTIMESTAMP() #大家只需要掌握一个函数就可以了
   FROM DUAL;
 
 
@@ -31,6 +31,51 @@ TO_DAYS(CURDATE()) - TO_DAYS(hire_date) "worked_days1"
 #雇用时间在1997年之后, department_id 为80 或 90 或110, commission_pct不为空
   SELECT last_name,hire_date,department_id
   FROM employees
-  WHERE department_id IN(80,90,110)
+  WHERE department_id IN (80,90,110)
   AND commission_pct IS NOT NULL
+  #and hire_date >= '1997-01-01';                               # 存在着隐式转换
+  #and  date_format(hire_date,'%Y-%m-%d') >= '1997-01-01';      # 显式转换操作,格式化: 日期---> 字符串
+  #and  date_format(hire_date,'%Y') >= '1997';                  # 显式转换操作, 格式化
+  AND hire_date >= STR_TO_DATE('1997-01-01','%Y-%m-%d');        # 显式转换操作, 解析: 字符串 ----> 日期
 
+
+# 7.查询公司中入职超过10000天的员工姓名, 入职时间
+  SELECT last_name,hire_date
+  FROM employees
+  WHERE DATEDIFF(CURDATE(),hire_date) >= 10000;
+
+
+# 8.做一个查询, 产生下面的结果
+#<last_name> earns <salary> monthly but wants <salary*3>
+
+  SELECT CONCAT(last_name,' earns ',TRUNCATE(salary,0), ' monthly but wants ',TRUNCATE(salary * 3,0)) "Dream Salary"
+  FROM employees;
+
+
+# 9.使用case-when, 按照下面的条件:
+/*job                  grade
+AD_PRES              	A
+ST_MAN               	B
+IT_PROG              	C
+SA_REP               	D
+ST_CLERK             	E
+
+产生下面的结果:
+*/
+
+
+  SELECT last_name "Last_name",job_id "Job_id",CASE job_id WHEN 'AD_PRES' THEN 'A'
+							 WHEN 'ST_MAN' THEN 'B'
+							 WHEN 'IT_PROG' THEN 'C'
+							 WHEN 'SA_REP' THEN 'D'
+							 WHEN 'ST_CLERK' THEN 'E'
+							 END "Grade"
+  FROM employees;
+
+  SELECT last_name "Last_name",job_id "Job_id",CASE job_id WHEN 'AD_PRES' THEN 'A'
+							 WHEN 'ST_MAN' THEN 'B'
+							 WHEN 'IT_PROG' THEN 'C'
+							 WHEN 'SA_REP' THEN 'D'
+							 WHEN 'ST_CLERK' THEN 'E'
+							 ELSE "undefined" END "Grade"
+  FROM employees;
