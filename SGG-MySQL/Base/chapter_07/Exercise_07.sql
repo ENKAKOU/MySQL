74d204312e591c0e4b5871666c0010d9d9fa4660
enkakou <enkakou0716@gmail.com>
2023-07-26 14:10:01 +0900

    Exercise_07

--- SGG-MySQL/Base/chapter_07/Exercise_07.sql
+++ SGG-MySQL/Base/chapter_07/Exercise_07.sql
@@ -1,5 +1,5 @@
 # 1.显示系统时间(注: 日期+时间)
-  SELECT NOW(),SYSDATE(),CURRENT_TIMESTAMP(),LOCALTIME(),LOCALTIMESTAMP() #大家只需要掌握一个函数就可以了
+  SELECT NOW(),SYSDATE(),CURRENT_TIMESTAMP(),LOCALTIME(),LOCALTIMESTAMP()
   FROM DUAL;
 
 
@@ -11,7 +11,7 @@
 # 3.将员工的姓名按首字母排序, 并写出姓名的长度(length)
   SELECT last_name,LENGTH(last_name) "name_length"
   FROM employees
-  #order by last_name asc;
+#order by last_name asc;
   ORDER BY name_length ASC;
 
 
@@ -28,7 +28,8 @@ TO_DAYS(CURDATE()) - TO_DAYS(hire_date) "worked_days1"
 
 
 # 6.查询员工姓名, hire_date, department_id, 满足以下条件:
-#雇用时间在1997年之后, department_id 为80 或 90 或110, commission_pct不为空
+#   雇用时间在1997年之后, department_id 为80 或 90 或110, commission_pct不为空
+
   SELECT last_name,hire_date,department_id
   FROM employees
   WHERE department_id IN (80,90,110)
@@ -45,15 +46,16 @@ TO_DAYS(CURDATE()) - TO_DAYS(hire_date) "worked_days1"
   WHERE DATEDIFF(CURDATE(),hire_date) >= 10000;
 
 
-# 8.做一个查询, 产生下面的结果
-#<last_name> earns <salary> monthly but wants <salary*3>
+# 8.做一个查询, 产生下面的结果:
+#   <last_name> earns <salary> monthly but wants <salary*3>
 
   SELECT CONCAT(last_name,' earns ',TRUNCATE(salary,0), ' monthly but wants ',TRUNCATE(salary * 3,0)) "Dream Salary"
   FROM employees;
 
 
 # 9.使用case-when, 按照下面的条件:
-/*job                  grade
+/*
+ job                  grade
 AD_PRES              	A
 ST_MAN               	B
 IT_PROG              	C
@@ -62,7 +64,6 @@ ST_CLERK             	E
 
 产生下面的结果:
 */
-
 
   SELECT last_name "Last_name",job_id "Job_id",CASE job_id WHEN 'AD_PRES' THEN 'A'
 							 WHEN 'ST_MAN' THEN 'B'
