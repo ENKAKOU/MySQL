a1314d7801ad91f3fd0ef75200fe58a74c8eb87c
enkakou <enkakou0716@gmail.com>
2023-08-04 11:50:58 +0900

    运算符

--- SGG-MySQL/Base/chapter_04/运算符.sql
+++ SGG-MySQL/Base/chapter_04/运算符.sql
@@ -5,44 +5,51 @@
   FROM DUAL;
 
 # 在SQL中, +没有连接的作用, 就表示加法运算.  此时,会将字符串转换为数值(隐式转换)
-  SELECT 100 + '1'  # 在Java语言中, 结果是: 1001.
+  SELECT 100 + '1'          # 在Java语言中, 结果是: 1001.
   FROM DUAL;
 
-  SELECT 100 + 'a' #此时将'a'看做0处理
+  SELECT 100 + 'a'          #此时将'a'看做0处理
   FROM DUAL;
 
-  SELECT 100 + NULL  # null值参与运算, 结果为null
+  SELECT 100 + NULL         # null值参与运算, 结果为null
   FROM DUAL;
 
-  SELECT 100, 100 * 1, 100 * 1.0, 100 / 1.0, 100 / 2, 100 + 2 * 5 / 2,100 / 3, 100 DIV 0  # 分母如果为0, 则结果为null
+  SELECT 100, 100 * 1, 100 * 1.0, 100 / 1.0, 100 / 2,
+  100 + 2 * 5 / 2,100 / 3, 100 DIV 0                   # 分母如果为0, 则结果为null
   FROM DUAL;
+
 
 # 取模运算:  % mod
   SELECT 12 % 3,12 % 5, 12 MOD -5,-12 % 5,-12 % -5
   FROM DUAL;
+
 
 #练习: 查询员工id为偶数的员工信息
   SELECT employee_id,last_name,salary
   FROM employees
   WHERE employee_id % 2 = 0;
 
+
+
 #2. 比较运算符
-#2.1 =  <=>  <> !=  <  <=  >  >=
+
+  #2.1 =  <=>  <> !=  <  <=  >  >=
 
 # = 的使用
-  SELECT 1 = 2,1 != 2,1 = '1',1 = 'a',0 = 'a' #字符串存在隐式转换. 如果转换数值不成功, 则看做0
+  SELECT 1 = 2,1 != 2,1 = '1',1 = 'a',0 = 'a'     #字符串存在隐式转换. 如果转换数值不成功, 则看做0
   FROM DUAL;
 
-  SELECT 'a' = 'a','ab' = 'ab','a' = 'b' #两边都是字符串的话, 则按照ANSI的比较规则进行比较.
+  SELECT 'a' = 'a','ab' = 'ab','a' = 'b'          #两边都是字符串的话, 则按照ANSI的比较规则进行比较.
   FROM DUAL;
 
-  SELECT 1 = NULL,NULL = NULL # 只要有null参与判断, 结果就为null
+  SELECT 1 = NULL,NULL = NULL                     #只要有null参与判断, 结果就为null
   FROM DUAL;
 
   SELECT last_name,salary,commission_pct
   FROM employees
   #where salary = 6000;
-  WHERE commission_pct = NULL;  #此时执行, 不会有任何的结果
+  WHERE commission_pct = NULL;                    #此时执行, 不会有任何的结果
+
 
 # <=>: 安全等于. 记忆技巧: 为NULL而生.
   SELECT 1 <=> 2,1 <=> '1',1 <=> 'a',0 <=> 'a'
@@ -50,6 +57,7 @@
 
   SELECT 1 <=> NULL, NULL <=> NULL
   FROM DUAL;
+
 
 #练习: 查询表中commission_pct为null的数据有哪些
   SELECT last_name,salary,commission_pct
@@ -59,7 +67,8 @@
   SELECT 3 <> 2,'4' <> NULL, '' != NULL,NULL != NULL
   FROM DUAL;
 
-#2.2
+
+  #2.2
   #1.IS NULL\ IS NOT NULL \ ISNULL
 
 #练习: 查询表中commission_pct为null的数据有哪些
@@ -110,10 +119,12 @@
 
   #4 in (set)\ not in (set)
 
+
 #练习: 查询部门为10,20,30部门的员工信息
   SELECT last_name,salary,department_id
   FROM employees
   WHERE department_id IN (10,20,30);
+
 
 #练习: 查询工资不是6000,7000,8000的员工信息
   SELECT last_name,salary,department_id
@@ -123,15 +134,18 @@
   #5 LIKE :模糊查询
 # % : 代表不确定个数的字符 (0个, 1个, 或多个)
 
+
 #练习: 查询last_name中包含字符'a'的员工信息
   SELECT last_name
   FROM employees
   WHERE last_name LIKE '%a%';
 
+
 #练习: 查询last_name中以字符'a'开头的员工信息
   SELECT last_name
   FROM employees
   WHERE last_name LIKE 'a%';
+
 
 #练习: 查询last_name中包含字符'a'且包含字符'e'的员工信息
   #写法1:
@@ -144,12 +158,15 @@
   FROM employees
   WHERE last_name LIKE '%a%e%' OR last_name LIKE '%e%a%';
 
+
 # _ : 代表一个不确定的字符
+
 
 #练习: 查询第3个字符是'a'的员工信息
   SELECT last_name
   FROM employees
   WHERE last_name LIKE '__a%';
+
 
 #练习: 查询第2个字符是_且第3个字符是'a'的员工信息
 #需要使用转义字符: \
@@ -169,6 +186,8 @@
 
   SELECT 'atguigu' REGEXP 'gu.gu','atguigu' REGEXP '[ab]'
   FROM DUAL;
+
+
 
 #3. 逻辑运算符:  OR ||  AND && NOT ! XOR
 
@@ -192,6 +211,7 @@
   WHERE department_id = 50 XOR salary > 6000;
 
 #注意:AND的优先级高于OR
+
 
 
 #4. 位运算符:  & |  ^  ~  >>   <<
